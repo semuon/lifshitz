05d0b855e05348bd9b4fb397d6573de7502d052e
Semeon Valgushev <semuon06@gmail.com>
2019-06-03 19:59:40 -0400

    +fixed operator +working now?

--- apps/hmc/main.cpp
+++ apps/hmc/main.cpp
@@ -76,7 +76,7 @@ double main_Action(const tPhysicalParams &params, const RealScalarFieldN &phi)
   {
     for(uint x = 0; x < vol; x++)
     {
-      res += (m2 - 2.0 * ndim * Z + 6.0 * ndim * invM2) * phi(x, i) * phi(x, i) / 2.0;
+      res += (m2 + 2.0 * ndim * Z + 6.0 * ndim * invM2) * phi(x, i) * phi(x, i) / 2.0;
 
       for(int mu = 0; mu < ndim; mu++)
       {
@@ -86,7 +86,7 @@ double main_Action(const tPhysicalParams &params, const RealScalarFieldN &phi)
         uint x2mu_bwd = lat.SiteIndexBackward(xmu_bwd, mu);
 
         res += phi(x, i) * invM2 * (phi(x2mu_fwd, i) + phi(x2mu_bwd, i)) / 2.0;
-        res += phi(x, i) * (Z - 4.0 * invM2) * (phi(xmu_fwd, i) + phi(xmu_bwd, i)) / 2.0;
+        res += phi(x, i) * (-Z - 4.0 * invM2) * (phi(xmu_fwd, i) + phi(xmu_bwd, i)) / 2.0;
       }
     }
   }
@@ -132,7 +132,7 @@ void main_HMCforce(const tPhysicalParams &params, const RealScalarFieldN &phi, R
   {
     for(uint x = 0; x < vol; x++)
     {
-      force(x, i) = (m2 - 2.0 * ndim * Z + 6.0 * ndim * invM2) * phi(x, i);
+      force(x, i) = (m2 + 2.0 * ndim * Z + 6.0 * ndim * invM2) * phi(x, i);
 
       for(int mu = 0; mu < ndim; mu++)
       {
@@ -142,7 +142,7 @@ void main_HMCforce(const tPhysicalParams &params, const RealScalarFieldN &phi, R
         uint x2mu_bwd = lat.SiteIndexBackward(xmu_bwd, mu);
 
         force(x, i) += invM2 * (phi(x2mu_fwd, i) + phi(x2mu_bwd, i));
-        force(x, i) += (Z - 4.0 * invM2) * (phi(xmu_fwd, i) + phi(xmu_bwd, i));
+        force(x, i) += (-Z - 4.0 * invM2) * (phi(xmu_fwd, i) + phi(xmu_bwd, i));
       }
     }
   }
@@ -191,10 +191,6 @@ void main_HMCevolve(const tPhysicalParams &params, const double dt, const int nu
   for(uint i = 0; i < n * vol; i++)
     pi[i] -= force[i] * dt;
 
-  //  pStdLogs.Write("\nforce = %2.15le\n", force.Norm());
-  //pStdLogs.Write("phi = %2.15le\n", phi.Norm());
-  //pStdLogs.Write("pi = %2.15le\n\n", pi.Norm());
-
   // Intermediate steps of leapfrog
   for(int step_idx = 0; step_idx < num_steps - 1; step_idx++)
   {
@@ -205,27 +201,21 @@ void main_HMCevolve(const tPhysicalParams &params, const double dt, const int nu
 
     for(uint i = 0; i < n * vol; i++)
       pi[i] -= force[i] * dt;
-
-  //    pStdLogs.Write("\nforce = %2.15le\n", force.Norm());
-  //pStdLogs.Write("phi = %2.15le\n", phi.Norm());
-  //pStdLogs.Write("pi = %2.15le\n\n", pi.Norm());
   }
 
   // Last step of leapfrog
   for(uint i = 0; i < n * vol; i++)
     phi[i] += pi[i] * dt / 2.0;
 
-  //  pStdLogs.Write("\nforce = %2.15le\n", force.Norm());
-  //pStdLogs.Write("phi = %2.15le\n", phi.Norm());
-  //pStdLogs.Write("pi = %2.15le\n\n", pi.Norm());
-
   pGlobalProfiler.StopTimer("HMC Trajectory");
 }
 
 int main(int argc, char **argv)
 {
-  const string f_bin_attr = "wb";
-  const string f_txt_attr = "w";
+  const string f_bin_write_attr = "wb";
+  const string f_bin_read_attr = "rb";
+  const string f_txt_write_attr = "w";
+  const string f_txt_read_attr = "r";
 
   common_AppInit(argc, argv, "Lifshitz regime: HMC");
   int64_t begin = Utils::GetTimeMs64();
@@ -255,15 +245,18 @@ int main(int argc, char **argv)
   double hmc_avg_dh = 0;
   double hmc_avg_exp_dh = 0;
 
+  VECTOR<double> action_history;
   VECTOR<double> dh_history;
   VECTOR<double> exp_dh_history;
 
+  action_history.reserve(hmc_num_conf);
   dh_history.reserve(hmc_num_conf);
   exp_dh_history.reserve(hmc_num_conf);
 
   tStartConfigurationType start_type = START_CONFIGURATION_RANDOM;
 
-  string fname_load_conf = "init.conf";
+  const std::string fname_hmc_stat = "hmc_stat.txt";
+  const std::string fname_load_conf = "init.conf";
 
   tPhysicalParams params;
   params.lambdaN = lambdaN;
@@ -272,15 +265,6 @@ int main(int argc, char **argv)
   params.Z = Z;
   params.N = n;
   params.kappa = kappa;
-
-  RealScalarFieldN phi(lat, n);
-  RealScalarFieldN force(lat, n);
-
-  for(uint idx = 0; idx < n * vol; idx++)
-    phi[idx] = (double) idx;
-
-  double action = main_Action(params, phi);
-  main_HMCforce(params, phi, force);
 
   RealScalarFieldN pi_field(lat, n);
   RealScalarFieldN phi_field_0(lat, n);
@@ -297,7 +281,7 @@ int main(int argc, char **argv)
     break;
     case START_CONFIGURATION_LOAD:
     {
-      FILE *f_load_conf = pDataDir.OpenFile(fname_load_conf, f_bin_attr);
+      FILE *f_load_conf = pDataDir.OpenFile(fname_load_conf, f_bin_read_attr);
       SAFE_FREAD(phi_field_0.DataPtr(), sizeof(double), vol * n, f_load_conf);
       fclose(f_load_conf);
     }
@@ -306,18 +290,18 @@ int main(int argc, char **argv)
     case START_CONFIGURATION_RANDOM:
     default:
       for(uint i = 0; i < n * vol; i++)
-        phi_field_0[i] = rand_gauss_double(0, 1);
+        phi_field_0[i] = rand_double(-1, 1);
     }
     break;
   }
 
   pStdLogs.Write("\n\nHMC BEGIN\n\n");
 
-  for(int traj_idx = 0; traj_idx < hmc_num_conf; traj_idx++)
+  for(int conf_idx = 0; conf_idx < hmc_num_conf; conf_idx++)
   {
     double h0 = 0;
     double h1 = 0;
-    double traj_action = 0;
+    double conf_action = 0;
 
     pGlobalProfiler.StartSection("HMC Trajectory");
 
@@ -333,74 +317,13 @@ int main(int argc, char **argv)
 
     h0 += main_Action(params, phi_field_1);
 
-    pStdLogs.Write("h0 = %2.15le\n", h0);
-
     main_HMCevolve(params, hmc_dt, hmc_num_steps, phi_field_1, pi_field);
-
-    //RealScalarFieldN ppp0(lat, n);
-    //RealScalarFieldN ppp1(lat, n);
-    //RealScalarFieldN ppp2(lat, n);
-    //RealScalarFieldN vvv0(lat, n);
-    //RealScalarFieldN vvv1(lat, n);
-    //RealScalarFieldN vvv2(lat, n);
-
-    //ppp0 = pi_field;
-    //vvv0 = phi_field_1;
-
-    //ppp2 = ppp0;
-    //vvv2 = vvv0;
-
-    /*pStdLogs.Write("\ninitial pi:\n");
-    for(uint i = 0; i < n * vol; i++)
-      pStdLogs.Write("%2.4le\t", ppp2[i]);
-    pStdLogs.Write("\n");
-    pStdLogs.Write("initial phi:\n");
-    for(uint i = 0; i < n * vol; i++)
-      pStdLogs.Write("%2.4le\t", vvv2[i]);
-    pStdLogs.Write("\n\n");*/
-
-    //main_HMCevolve(params, hmc_dt, hmc_num_steps, vvv2, ppp2);
-
-    /*pStdLogs.Write("final pi:\n");
-    for(uint i = 0; i < n * vol; i++)
-      pStdLogs.Write("%2.4le\t", ppp2[i]);
-    pStdLogs.Write("\n");
-    pStdLogs.Write("final phi:\n");
-    for(uint i = 0; i < n * vol; i++)
-      pStdLogs.Write("%2.4le\t", vvv2[i]);
-    pStdLogs.Write("\n\n");*/
-
-    //ppp1 = ppp2;
-    //vvv1 = vvv2;
-
-    //for(uint i = 0; i < n * vol; i++)
-    //  ppp2[i] = -1.0 * ppp2[i];
-
-    //main_HMCevolve(params, hmc_dt, hmc_num_steps, vvv2, ppp2);
-
-    /*pStdLogs.Write("back pi:\n");
-    for(uint i = 0; i < n * vol; i++)
-      pStdLogs.Write("%2.4le\t", ppp2[i]);
-    pStdLogs.Write("\n");
-    pStdLogs.Write("back phi:\n");
-    for(uint i = 0; i < n * vol; i++)
-      pStdLogs.Write("%2.4le\t", vvv2[i]);
-    pStdLogs.Write("\n\n");*/
-
-    //pStdLogs.Write("\n|pi0 - pi2| = %2.15le\n\n", (ppp0 + ppp2).Norm());
-    //pStdLogs.Write("\n|pi1 - pi2| = %2.15le\n\n", (ppp1 + ppp2).Norm());
-    //pStdLogs.Write("\n|phi0 - phi2| = %2.15le\n\n", (vvv0 - vvv2).Norm());
-    //pStdLogs.Write("\n|phi1 - phi2| = %2.15le\n\n", (vvv1 - vvv2).Norm());
 
     for(uint i = 0; i < n * vol; i++)
       h1 += pi_field[i] * pi_field[i] / 2.0;
 
-    traj_action = main_Action(params, phi_field_1);
-
-    pStdLogs.Write("h1 pi = %2.15le\n", h1);
-    pStdLogs.Write("h1 action = %2.15le\n", traj_action);
-
-    h1 += traj_action;
+    conf_action = main_Action(params, phi_field_1);
+    h1 += conf_action;
 
     // Accept/reject step
     double hmc_dh = h1 - h0;
@@ -415,10 +338,10 @@ int main(int argc, char **argv)
       double alpha = exp(-hmc_dh);
       double dice = rand_double(0, 1);
 
-      accepted = (alpha > dice);
+      accepted = (dice < alpha);
     }
 
-    pStdLogs.Write("step = %d\n", traj_idx + 1);
+    pStdLogs.Write("conf = %d\n", conf_idx + 1);
 
     if(accepted)
     {
@@ -426,16 +349,14 @@ int main(int argc, char **argv)
 
       hmc_accept_rate += 1.0 / hmc_num_conf;
 
+      action_history.push_back(conf_action / vol);
       dh_history.push_back(hmc_dh);
       exp_dh_history.push_back(exp(-hmc_dh));
-
-      //hmc_avg_dh = (hmc_avg_dh * hmc_num_accepted + hmc_dh) / (hmc_num_accepted + 1.0);
-      //hmc_avg_exp_dh = (hmc_avg_exp_dh * hmc_num_accepted + exp(-hmc_dh)) / (hmc_num_accepted + 1.0);
 
       hmc_num_accepted++;
 
       pStdLogs.Write("status: ACCEPTED\n");
-      pStdLogs.Write("action = %2.15le\n", traj_action);
+      pStdLogs.Write("action = %2.15le\n", conf_action);
     }
     else
     {
@@ -451,15 +372,17 @@ int main(int argc, char **argv)
 
   pStdLogs.Write("\n\nHMC COMPLETED\n\n");
 
-  pStdLogs.Write("Num. accepted: %d\n", hmc_num_accepted);
+  pStdLogs.Write("Num. accepted: %d / %d\n", hmc_num_accepted, hmc_num_conf);
   pStdLogs.Write("Acceptance rate: %2.15le\n", hmc_accept_rate);
   pStdLogs.Write("<exp(dh)> = %2.15le +/- %2.15le\n", main_VectorMean(exp_dh_history), main_VectorSigma(exp_dh_history));
   pStdLogs.Write("<dh> = %2.15le +/- %2.15le\n\n", main_VectorMean(dh_history), main_VectorSigma(dh_history));
 
-  pStdLogs.Write("Action: %2.15le", action);
-  FILE *f = pDataDir.OpenFile("test.bin", "wb");
-  Formats::DumpBinary(f, force);
-  fclose(f);
+  FILE *f_hmc_stat = pDataDir.OpenFile(fname_hmc_stat, f_txt_write_attr);
+
+  for(int i = 0; i < hmc_num_accepted; i++)
+    SAFE_FPRINTF(f_hmc_stat, "%2.15le\t%2.15le\t%2.15le\n", action_history[i], exp_dh_history[i], dh_history[i]);
+
+  fclose(f_hmc_stat);
 
   int64_t end = Utils::GetTimeMs64();
 
